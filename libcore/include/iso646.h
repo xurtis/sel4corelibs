b389c0054ddced0968770b55f64337afb66006bd
Curtis Millar <curtis@curtism.me>
2020-10-03 16:49:28 +1000

    Add <iso646.h>
    Signed-off-by: Curtis Millar <curtis@curtism.me>

---
+++ libcore/include/iso646.h
@@ -0,0 +1,23 @@
+/*
+ * Copyright 2020, Data61, CSIRO (ABN 41 687 119 230)
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+/*
+ * ISO/IEC 9899:2018 7.9 - Alternative spellings
+ */
+
+#pragma once
+
+#define and    &&
+#define and_eq &=
+#define bitand &
+#define bitor  |
+#define compl  ~
+#define not    !
+#define not_eq !=
+#define or     ||
+#define or_eq  |=
+#define xor    ^
+#define xor_eq ^=
