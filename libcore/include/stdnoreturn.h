e6c2a81eabad14ee9504bcfc116d13ee98856c83
Curtis Millar <curtis@curtism.me>
2020-10-03 18:35:59 +1000

    Add <stdnoreturn.h>
    Signed-off-by: Curtis Millar <curtis@curtism.me>

---
+++ libcore/include/stdnoreturn.h
@@ -0,0 +1,13 @@
+/*
+ * Copyright 2020, Data61, CSIRO (ABN 41 687 119 230)
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+/*
+ * ISO/IEC 9899:2018 7.23 - _Noreturn
+ */
+
+#pragma once
+
+#define noreturn _Noreturn
