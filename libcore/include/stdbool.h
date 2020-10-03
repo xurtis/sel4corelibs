bd85ee6941252e59b7538f853a0cd0133a390bea
Curtis Millar <curtis@curtism.me>
2020-10-03 18:02:10 +1000

    Add <stdbool.h>
    Signed-off-by: Curtis Millar <curtis@curtism.me>

---
+++ libcore/include/stdbool.h
@@ -0,0 +1,17 @@
+/*
+ * Copyright 2020, Data61, CSIRO (ABN 41 687 119 230)
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+/*
+ * ISO/IEC 9899:2018 7.18 - Boolean type and values
+ */
+
+#pragma once
+
+#define bool  _Bool
+#define true  1
+#define false 0
+
+#define __bool_true_false_are_defined 1
