d516d8231341ac6305db2249d143873a1955c0e7
Curtis Millar <curtis@curtism.me>
2020-10-03 16:55:15 +1000

    Add <stdalign.h>
    Signed-off-by: Curtis Millar <curtis@curtism.me>

---
+++ libcore/include/stdalign.h
@@ -0,0 +1,17 @@
+/*
+ * Copyright 2020, Data61, CSIRO (ABN 41 687 119 230)
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+/*
+ * ISO/IEC 9899:2018 7.15 - Alignment
+ */
+
+#pragma once
+
+#define alignas _Alignas
+#define alignof _Alignof
+
+#define __alignas_is_defined 1
+#define __alignof_is_defined 1
